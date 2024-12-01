ebe2434be16c1df2b5996595311ea5a697fe5332
sunhyeongpp <parksunhyeong529@gmail.com>
2024-12-01 20:19:33 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,17 +1,13 @@
 function solution(n, arr1, arr2) {
-    var answer = [];
-    
-    // 배열의 각 요소를 2진법 문자열로 변환
-    const newArr1 = arr1.map(value=> value.toString(2).padStart(n,"0"));
-    const newArr2 = arr2.map(value=> value.toString(2).padStart(n,"0"));
-    
-    for(let i =0; i<n; i++){
-        let line = "";
-        for(let j=0; j<n; j++){
-            line+= (newArr1[i][j]==="1" || newArr2[i][j]==="1")? "#" : " ";
-        }
-        answer.push(line);
-    }
-    return answer;
+    return arr1.map((num1, i) => {
+    // 두 숫자의 비트 OR 연산 결과를 2진 문자열로 변환
+  const binary = (num1 | arr2[i]).toString(2).padStart(n, '0');
+        
+        // 각 문자를 조건에 따라 '#' 또는 ' '로 변환
+        return binary
+            .split("")
+            .map(char => (char === "1" ? "#" : " "))
+            .join("");
+    });
 }
 
