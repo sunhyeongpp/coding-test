58f9424bb8dd7a869b0f99c18a153118b01474a0
sunhyeongpp <parksunhyeong529@gmail.com>
2024-12-02 16:29:46 +0900

    [level 1] Title: 명예의 전당 (1), Time: 4.05 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,16 +1,16 @@
 function solution(k, score) {
-    const scoreArr = [];
+    const scoreArr = []; // 명예의 전당 점수 배열
     const answer = [];
     
     for(let i=0; i<score.length; i++){
-        scoreArr.push(score[i]);
-        scoreArr.sort((a,b)=>b-a);
+        scoreArr.push(score[i]); 
+        scoreArr.sort((a,b)=>b-a); // 내림차순 정렬
         
-        if(scoreArr.length>k){
+        if(scoreArr.length>k){ // 출연자가 k명이 넘을 때매다 탈락자를 pop 시킴
             scoreArr.pop();
         }
         
-        answer.push(scoreArr[scoreArr.length-1]);
+        answer.push(scoreArr[scoreArr.length-1]); // 명예의전당 중 최하위를 push
     }   
     return answer;
 }
