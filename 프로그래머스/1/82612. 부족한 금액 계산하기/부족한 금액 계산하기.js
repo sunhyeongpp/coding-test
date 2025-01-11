f9781a7fd9fcbdcf27ff033d7d023f3a7457a607
박선형 <parksunhyeong529@gmail.com>
2025-01-11 20:43:21 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,14 @@
+function solution(price, money, count) {
+    var answer = -1;
+
+    let countPrice = 0;
+    for(let i=1; i<=count; i++){
+    countPrice += i * price;  
+    } 
+    if(money >= countPrice){
+        answer = 0;
+    } else{
+        answer = countPrice-money;
+    }
+    return answer;
+}
