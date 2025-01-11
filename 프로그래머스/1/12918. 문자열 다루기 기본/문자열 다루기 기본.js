f863a875ea93554f69c2feaf4d7567b836ce0d0a
박선형 <parksunhyeong529@gmail.com>
2025-01-11 21:07:54 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -7,4 +7,10 @@ function solution(s) {
    return s.length===6 ||s.length===4 && !isNaN(s)
 }
 =>"12e3"은 지수표기법이라 isNaN(s)가 false여서 한계가 있음.
+
+isNaN이 숫자 아닌 경우에도 false 반환하는 경우
+1) " " (공백): 공백은 숫자 0으로 변환되므로 isNaN은 false를 반환
+2) boolean 값: true와 false는 각각 숫자 1과 0으로 변환되므로 isNaN은 false를 반환
+3) null: 숫자 0으로 변환되므로 isNaN은 false를 반환
+
 */
