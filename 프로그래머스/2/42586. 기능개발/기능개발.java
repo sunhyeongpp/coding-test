000be004a6bb9c9f6dab3af14efdb609f5de2a9e
박선형 <parksunhyeong529@gmail.com>
2025-06-21 15:16:55 +0900

    [level 2] Title: 기능개발, Time: 7.93 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,44 +2,36 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
+      
         
-        Queue<Integer> queue = new LinkedList<>();
-        List<Integer> answer = new ArrayList<>();
+        List<Integer> answerList = new ArrayList<>();
         
-        for(int i=0; i<progresses.length; i++){
-            if((100-progresses[i])%speeds[i]==0){
-                queue.offer((100-progresses[i])/speeds[i]);
+        Queue<Integer> que = new LinkedList<>();
+        
+        for(int i=0; i<speeds.length; i++){
+            int lastTime = 100-progresses[i];
+            if(lastTime%speeds[i]==0){
+                que.add(lastTime/speeds[i]);
             }else{
-                queue.offer((100-progresses[i])/speeds[i]+1);
+                que.add(lastTime/speeds[i]+1);
             }
         }
-               
-        int now = queue.poll();
-        int count = 1;
-               
-               while(!queue.isEmpty()){
-                   if(now >= queue.peek()){
-                       count++;
-                       queue.poll();
-                   } else{
-                       answer.add(count);
-                       count = 1;
-                       now = queue.poll();
-                   }
-               }
-        answer.add(count);
-               
-               return answer.stream().mapToInt(i->i).toArray();
         
+        int cnt = 1;
+        int prev = que.poll();
+        while(!que.isEmpty()){
+            int cur = que.poll();
+            if(cur<=prev){
+                cnt++;
+            }else{
+                answerList.add(cnt);
+                cnt = 1;
+                prev = cur;
+            }
+        }
+        
+        answerList.add(cnt);
+        
+        return answerList.stream().mapToInt(i->i).toArray();
     }
 }
-
-/*
-1. 각 작업이 완료되기까지 필요 나라 큐에 저장
-2. 큐에서 값을 꺼내 기준 날짜로 삼음
-3. 다른 작업들도 이 기준 날짜 안에 배포 가능한지 비교
-4. 가능하면 함께 배포
-5. 불가능하면 카운트 저장, 다음 작업부터 새롭게 시작
-6. 마지막까지 처리 후 리스트를 배열로 변환
-
-*/
