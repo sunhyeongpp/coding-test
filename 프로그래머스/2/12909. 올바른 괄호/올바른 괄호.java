d45a46bc60f702e0f85e47a86624b07306aca975
박선형 <parksunhyeong529@gmail.com>
2025-06-21 16:02:23 +0900

    [level 2] Title: 올바른 괄호, Time: 15.70 ms, Memory: 55.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,17 +2,33 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        Stack<Character> stack = new Stack<>();
+        Deque<Character> deque = new ArrayDeque<>();
+        
+        // if(s.toCharArray()[0]==')') {
+        //     return false;
+        // } else if(s.toCharArray()[0]=='('){    
+        //      for(char ch: s.toCharArray()){
+        //     if(deque.isEmpty()&&ch=='('){
+        //         deque.addFirst(ch);
+        //         } else if(deque.isEmpty()&&ch==')'){
+        //         return false;
+        //         } else if(deque.peekLast()=='('&&ch==')'){
+        //         deque.removeLast();
+        //         }
+        //     }
+        // }
         
         for(char ch : s.toCharArray()){
-            if(ch =='('){
-                stack.push(ch);
-            } else if(ch ==')'){
-                if(stack.isEmpty()) return false;
-                stack.pop();
+            if(ch=='('){
+                deque.addLast(ch);
+            } else{
+                if(deque.isEmpty()){
+                    return false;
+                }
+                deque.removeLast();
             }
         }
         
-        return stack.isEmpty();
+        return deque.isEmpty();
     }
 }
