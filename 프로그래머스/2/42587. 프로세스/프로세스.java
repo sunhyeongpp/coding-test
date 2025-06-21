7b0aed03815b9e0f8502056849c30bff341d7c63
박선형 <parksunhyeong529@gmail.com>
2025-06-21 21:16:10 +0900

    [level 2] Title: 프로세스, Time: 1.62 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,30 +2,38 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        Queue<int[]> queue = new LinkedList<>();
+   
         
-        for(int i=0; i<priorities.length; i++){
-            queue.add(new int[]{i, priorities[i]});
+        //priorities큐
+        Queue <int[]> que = new LinkedList<>();
+        for(int i=0; i<priorities.length;i++){
+            que.add(new int[]{i, priorities[i]});
         }
         
-        int order = 0;
-        while(!queue.isEmpty()){
-            int[] current = queue.poll();
-            boolean hasHigher = false;
-            
-            for(int[] q: queue){
-                if(q[1]>current[1]){
-                    hasHigher = true;
-                    break;
-                }
-            }         
-            if(hasHigher){
-                queue.add(current);
-            } else{
-                order++;
-                if(current[0]==location) return order;
-            }         
-        }
-        return -1;       
+       
+        int count = 0;
+       while(!que.isEmpty()){
+            int[] current = que.poll();
+           boolean higher = false;
+           
+           for(int[] q: que){
+               if(q[1]>current[1]){
+                  higher = true;
+                   break;
+               }
+           }
+           
+           if(higher){
+               que.add(current);
+           }else{
+               count++;
+               if(current[0]==location) return count;
+           }
+           
+           
+       }       
+        
+        
+        return -1;
     }
 }
